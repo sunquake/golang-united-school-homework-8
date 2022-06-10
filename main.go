6afb1d38ec12788da24bfe7ec9693dfc71cce273
sunquake <kaskyr@gmail.com>
2022-06-11 00:31:59 +0600

    v1

--- main.go
+++ main.go
@@ -1,11 +1,90 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
+type Arguments map[string]string
+
+type Item struct {
+	Id    string `json:"id"`
+	Email string `json:"email"`
+	Age   int    `json:"age"`
+}
+
+var item Item
+
 func Perform(args Arguments, writer io.Writer) error {
+	oper := args["operation"]
+	if oper == "" {
+		return errors.New("-operation flag has to be specified")
+	}
+	if args["fileName"] == "" {
+		return errors.New("-fileName flag has to be specified")
+	}
+
+	file, _ := os.OpenFile(args["fileName"], os.O_RDWR|os.O_CREATE, 0666)
+	defer file.Close()
+	b, _ := ioutil.ReadAll(file)
+	items := []Item{}
+	json.Unmarshal(b, &items)
+	switch oper {
+	case "add":
+		if args["item"] == "" {
+			return errors.New("-item flag has to be specified")
+		}
+		for _, v := range items {
+			if v.Id == item.Id {
+				return fmt.Errorf("Item with id %s already exists", v.Id)
+			}
+		}
+		items = append(items, item)
+		js, _ := json.Marshal(items)
+
+		er := file.Truncate(0)
+		fmt.Println(er)
+		fmt.Fprint(file, js)
+	case "list":
+		_, err := writer.Write(b)
+		return err
+	case "findById":
+		for _, v := range items {
+			if v.Id == item.Id {
+				js, _ := json.Marshal(v)
+				_, err := writer.Write(js)
+				return err
+			}
+		}
+	case "remove":
+		temp := []Item{}
+		for _, v := range items {
+			if v.Id != item.Id {
+				temp = append(temp, v)
+			}
+		}
+		js, _ := json.Marshal(temp)
+		er := file.Truncate(0)
+		fmt.Println(er)
+		fmt.Fprint(file, js)
+	default:
+		return fmt.Errorf("Operation %s not allowed!", oper)
+	}
+	return nil
+}
+
+func parseArgs() Arguments {
+	op := flag.String("operation", "", "")
+	itm := flag.String("item", "", "a json")
+	fN := flag.String("fileName", "", "")
+	flag.Parse()
+	json.Unmarshal([]byte(*itm), &item)
+	return Arguments{"operation": *op, "id": item.Id, "item": *itm, "fileName": *fN}
 }
 
 func main() {
